1f41de07101637fd0c4f10db27a15e1c27bd218c
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-05-21 19:34:58 +0100

    chore: Update file paths for components in app directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@smolpack/bootstrap-extensions/dist/css/bootstrap-extensions.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Header from '../components/Header';
+import Header from './components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
