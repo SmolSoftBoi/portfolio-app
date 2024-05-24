ae8533800e0fbe8eb87b15283bee721ec1f045d8
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-05-24 19:28:01 +0100

    chore: Update next.config.js for trailing slash configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirects: true,
   images: {
     unoptimized: true
   }
