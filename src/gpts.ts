9bfcd02d82ab320ed3b33fb893a1a258c5bc8743
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-05-27 15:28:38 +0100

    feat: Add pack property to Gpt interface and update default gpts array

--- src/gpts.ts
+++ src/gpts.ts
@@ -2,6 +2,7 @@ export interface Gpt {
   id: string;
   title: string;
   description: string;
+  pack: string;
 }
 
 const gpts: Gpt[] = [
@@ -9,11 +10,13 @@ const gpts: Gpt[] = [
     id: '20gHoPEL3',
     title: 'Apparel Development Assistant',
     description: 'Expert in apparel.',
+    pack: 'Product Development',
   },
   {
     id: 'aqJNIv5y2',
     title: 'Blog Content Creater',
     description: 'I assist with new blog content, from name to content.',
+    pack: 'Blog Manager',
   },
 ];
 
