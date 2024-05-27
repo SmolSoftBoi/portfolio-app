38d7c85f4fe6b34716eb8f5dcd6e4f2f79f0d1e6
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-05-27 14:24:58 +0100

    feat: Add Gpt interface and default gpts array

--- src/gpts.ts
+++ src/gpts.ts
@@ -1,5 +1,20 @@
-export interface Gpt {}
+export interface Gpt {
+  id: string;
+  title: string;
+  description: string;
+}
 
-const gpts: Gpt[] = [];
+const gpts: Gpt[] = [
+  {
+    id: '20gHoPEL3',
+    title: 'Apparel Development Assistant',
+    description: 'Expert in apparel.'
+  },
+  {
+    id: 'aqJNIv5y2',
+    title: 'Blog Content Creater',
+    description: 'I assist with new blog content, from name to content.'
+  },
+];
 
 export default gpts;
