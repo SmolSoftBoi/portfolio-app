9dc092066939573f773b09810bdb72c83290f932
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-06-13 23:17:05 +0100

    feat: Add Yodeyma to default gpts array

--- src/gpts.ts
+++ src/gpts.ts
@@ -3,6 +3,7 @@ export interface Gpt {
   title: string;
   description: string;
   pack: string;
+  a?: string;
 }
 
 const gpts: Gpt[] = [
@@ -12,12 +13,14 @@ const gpts: Gpt[] = [
     description:
       'Creates consistent, efficient, customised social media strategies.',
     pack: 'Social Media Manager',
+    a: 'SL',
   },
   {
     id: '20gHoPEL3',
     title: 'Apparel Development Assistant',
     description: 'Expert in apparel.',
     pack: 'Product Development',
+    a: 'SL',
   },
   {
     id: 'M4coYtWOQ',
@@ -25,24 +28,28 @@ const gpts: Gpt[] = [
     description:
       'Streamlines article creation with clear language and cohesive structure.',
     pack: 'Research',
+    a: 'SL',
   },
   {
     id: 'aqJNIv5y2',
     title: 'Blog Content Creater',
     description: 'I assist with new blog content, from name to content.',
     pack: 'Blog Manager',
+    a: 'SL',
   },
   {
     id: 'CXqmylFwS',
     title: 'Brand Builder',
     description: 'Brand expert focused on research and optimisation.',
     pack: 'Brand Builder',
+    a: 'SL',
   },
   {
     id: 'XTtiergjL',
     title: 'Business Formation Assistant',
     description: 'Expert in forming tailored businesses.',
     pack: 'Business Assistant',
+    a: 'SL',
   },
   {
     id: 'wcbOFpwOc',
@@ -50,18 +57,21 @@ const gpts: Gpt[] = [
     description:
       'Expert in business planning and communication, with a focus on clarity and professionalism.',
     pack: 'Business Assistant',
+    a: 'SL',
   },
   {
     id: '',
     title: 'Data Analysis',
     description: 'Expert in data analysis, tailored to specific roles.',
     pack: 'Data Analysis',
+    a: 'SL',
   },
   {
     id: 'zO9EapKW4',
     title: 'Email Prospecting Assistant',
     description: 'Friendly, detailed guide for crafting effective emails.',
     pack: 'Sales',
+    a: 'SL',
   },
   {
     id: 'FmjMUyGxS',
@@ -69,30 +79,35 @@ const gpts: Gpt[] = [
     description:
       'Efficient and insightful reviewer with customisable reporting.',
     pack: 'Research',
+    a: 'SL',
   },
   {
     id: '3t54PuDS5',
     title: 'Poster Development Assistant',
     description: 'Poster strategy expert.',
     pack: 'Product Development',
+    a: 'SL',
   },
   {
     id: 'coAMcvFvn',
     title: 'Printful Catalog',
     description: "Expert guide for exploring Printful's catalog.",
     pack: 'Actions',
+    a: 'SL',
   },
   {
     id: '4itFNo6xX',
     title: 'Process Analysis',
     description: 'Creative assistant for refining processes.',
     pack: 'Data Analysis',
+    a: 'SL',
   },
   {
     id: 'R1TDgI5ww',
     title: 'Product Label Development Assistant',
     description: 'I create unique product label ideas for specific audiences.',
     pack: 'Product Development',
+    a: 'SL',
   },
   {
     id: '8riJxcbrM',
@@ -100,24 +115,28 @@ const gpts: Gpt[] = [
     description:
       'I create research questions, analysing and reimagining them creatively.',
     pack: 'Research',
+    a: 'SL',
   },
   {
     id: 'mnMCGKCR7',
     title: 'Sales Role Play Assistant',
     description: 'Brainstorms sales ideas.',
     pack: 'Sales',
+    a: 'SL',
   },
   {
     id: 'CDG1Dq2Db',
     title: 'Sales Script Assistant',
     description: 'Expert in scripts and content creation for sales.',
     pack: 'Sales',
+    a: 'SL',
   },
   {
     id: 'pehj541mM',
     title: 'Sales Strategy Assistant',
     description: 'A sales strategy assistant for ideation and planning.',
     pack: 'Sales',
+    a: 'SL',
   },
   {
     id: 'kz6fvnGz4',
@@ -125,18 +144,27 @@ const gpts: Gpt[] = [
     description:
       'Expert at enhancing social media calendars with flair and clarity across industries.',
     pack: 'Social Media Manager',
+    a: 'SL',
   },
   {
     id: '86hnq4Sqi',
     title: 'Social Media Content Creator',
     description: 'Expert at crafting standout social media content.',
     pack: 'Social Media Manager',
+    a: 'SL',
   },
   {
     id: 'cJDB5iDJ5',
     title: 'Social Media Growth Coach',
     description: 'Crafts personalised social media coaching.',
     pack: 'Social Media Manager',
+    a: 'SL',
+  },
+  {
+    id: 'q8gkKMGNs',
+    title: 'Yodeyma',
+    description: 'Focused on Yodeyma, and inspiring positive change.',
+    pack: 'Shopping',
   },
 ];
 
